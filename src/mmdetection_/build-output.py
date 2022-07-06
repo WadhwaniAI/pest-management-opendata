e12ea1e8e9c6c9a08215e208a4d0ad8b7d25fd93
Jerome White <jerome@wadhwaniai.org>
2022-07-06 15:30:39 +0530

    Updates for S3 URLs in the CSV; custom JSON encoder to ease processing

--- src/mmdetection_/build-output.py
+++ src/mmdetection_/build-output.py
@@ -1,11 +1,10 @@
 import sys
 import json
-import logging
-import itertools as it
+import functools as ft
 import collections as cl
 from pathlib import Path
 from argparse import ArgumentParser
-from multiprocessing import Pool
+from multiprocessing import Pool, Queue
 
 import pandas as pd
 from shapely import wkt
@@ -15,50 +14,73 @@ from lib import Logger, ImageInfo, LabelClass
 
 Ann = cl.namedtuple('Ann', 'bboxes, labels')
 
+class PandasEncoder(json.JSONEncoder):
+    @ft.singledispatchmethod
+    def default(self, o):
+        return super().default(o)
+
+    @default.register
+    def _(self, o: pd.DataFrame):
+        return o.to_numpy().tolist()
+
+    @default.register
+    def _(self, o: pd.Series):
+        return o.tolist()
+
 #
 #
 #
-def func(args):
-    (image, df, categories) = args
-    Logger.info(image)
+def func(incoming, outgoing, args):
+    while True:
+        (image, df, categories) = incoming.get()
+        Logger.info(image)
 
-    info = ImageInfo(image)
-    record = dict(info.shape(), filename=str(info))
+        info = ImageInfo(image, args.data_root)
+        record = dict(info.shape(), filename=str(info))
 
-    view = df.dropna(subset=['geometry'])
-    if view.empty:
-        ann = Ann([], [])
-    else:
-        geometry = view['geometry'].apply(wkt.loads)
-        gdf = GeoDataFrame(view, geometry=geometry)
-        ann = Ann(
-            gdf.geometry.bounds.to_numpy(),
-            gdf['label'].replace(categories).to_numpy(),
-        )
-    record['ann'] = ann._asdict()
+        view = df.dropna(subset=['geometry'])
+        if view.empty:
+            ann = Ann([], [])
+        else:
+            geometry = view['geometry'].apply(wkt.loads)
+            gdf = GeoDataFrame(view, geometry=geometry)
+            ann = Ann(
+                gdf.geometry.bounds,
+                gdf['label'].replace(categories),
+            )
+        record['ann'] = ann._asdict()
 
-    return record
-
-def each(fp, args):
-    df = pd.read_csv(fp)
-
-    lc = LabelClass(df)
-    categories = dict(lc.l2c())
-    if args.split:
-        df = df.query('split in @args.split')
-
-    for (i, g) in df.groupby('path', sort=False):
-        if args.image_root is not None:
-            i = args.image_root.joinpath(i)
-        yield (i, g, categories)
+        outgoing.put(record)
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
-    arguments.add_argument('--image-root', type=Path)
+    arguments.add_argument('--data-root', type=Path)
     arguments.add_argument('--split', action='append')
     arguments.add_argument('--workers', type=int)
     args = arguments.parse_args()
 
-    with Pool(args.workers) as pool:
-        data = pool.imap_unordered(func, each(sys.stdin, args))
-        json.dump(list(data), sys.stdout)
+    incoming = Queue()
+    outgoing = Queue()
+    initargs = (
+        outgoing,
+        incoming,
+        args,
+    )
+
+    with Pool(args.workers, func, initargs):
+        df = pd.read_csv(sys.stdin)
+
+        lc = LabelClass(df)
+        categories = dict(lc.l2c())
+        if args.split:
+            df = df.query('split in @args.split')
+
+        groups = df.groupby('url', sort=False)
+        for i in groups:
+            outgoing.put((*i, categories))
+
+        data = []
+        for _ in range(groups.ngroups):
+            record = incoming.get()
+            data.append(record)
+        json.dump(data, sys.stdout, cls=PandasEncoder)
