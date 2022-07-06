108fd98755f1b8a048899494b63f2e5b36d7127b
Jerome White <jerome@wadhwaniai.org>
2022-07-06 14:48:57 +0530

    Updates to support data paths specified as URLs

--- src/detectron2_/build-output.py
+++ src/detectron2_/build-output.py
@@ -1,18 +1,13 @@
 import sys
 import json
-import logging
-import itertools as it
-import collections as cl
 from pathlib import Path
 from argparse import ArgumentParser
-from multiprocessing import Pool
+from multiprocessing import Pool, Queue
 
 import pandas as pd
 from detectron2.structures import BoxMode
 
 from lib import Logger, ImageInfo, LabelClass, BoxIterator
-
-from things import read_things
 
 class DetectronBoxes(BoxIterator):
     def __init__(self, df, categories):
@@ -32,41 +27,51 @@ class DetectronBoxes(BoxIterator):
 #
 #
 #
-def func(args):
-    (image, df, categories) = args
-    Logger.info(image)
+def func(incoming, outgoing, args):
+    while True:
+        (image, df, categories) = incoming.get()
+        Logger.info(image)
 
-    info = ImageInfo(image)
-    boxes = DetectronBoxes(df, categories)
-    record = {
-        'image_id': int(boxes),
-        'file_name': str(info),
-        'annotations': list(boxes),
-    }
-    record.update(info.shape())
+        info = ImageInfo(image, args.data_root)
+        boxes = DetectronBoxes(df, categories)
+        record = {
+            'image_id': int(boxes),
+            'file_name': str(info),
+            'annotations': list(boxes),
+        }
+        record.update(info.shape())
 
-    return record
-
-def each(fp, args):
-    df = pd.read_csv(fp)
-
-    lc = LabelClass(df)
-    categories = dict(lc.l2c())
-    if args.split:
-        df = df.query('split in @args.split')
-
-    for (i, g) in df.groupby('path', sort=False):
-        if args.image_root is not None:
-            i = args.image_root.joinpath(i)
-        yield (i, g, categories)
+        outgoing.put(record)
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
-    arguments.add_argument('--image-root', type=Path)
+    arguments.add_argument('--data-root', type=Path)
     arguments.add_argument('--split', action='append')
     arguments.add_argument('--workers', type=int)
     args = arguments.parse_args()
 
-    with Pool(args.workers) as pool:
-        data = pool.imap_unordered(func, each(sys.stdin, args))
-        json.dump(list(data), sys.stdout)
+    incoming = Queue()
+    outgoing = Queue()
+    initargs = (
+        outgoing,
+        incoming,
+        args,
+    )
+
+    with Pool(args.workers, func, initargs):
+        df = pd.read_csv(sys.stdin)
+
+        lc = LabelClass(df)
+        categories = dict(lc.l2c())
+        if args.split:
+            df = df.query('split in @args.split')
+
+        groups = df.groupby('url', sort=False)
+        for i in groups:
+            outgoing.put((*i, categories))
+
+        data = []
+        for _ in range(groups.ngroups):
+            record = incoming.get()
+            data.append(record)
+        json.dump(data, sys.stdout)
