2e4f7012326e0b1d42d8a0fbf70b912173252e20
Jerome White <jerome@wadhwaniai.org>
2022-07-06 14:52:22 +0530

    Support for CLI
    Alleviates the need to perform some commands manually.

--- bin/to-wadhwaniai.sh
+++ bin/to-wadhwaniai.sh
@@ -5,9 +5,26 @@ ROOT=`git rev-parse --show-toplevel`
 export PYTHONPATH=$ROOT:$PYTHONPATH
 export PYTHONLOGLEVEL=info
 
-meta=( `readlink --canonicalize $ROOT/data/metadata/*` )
-source=`sed -e's/ / --source /g' <<< ${sources[@]}`
+while getopts 'd:v:h' option; do
+    case $option in
+	d) data=$OPTARG ;;
+        v) version=$OPTARG ;;
+        h)
+	    cat <<EOF
+Usage
+./$0 -v [version]
+EOF
+            exit 0
+            ;;
+        *) echo -e Unrecognized option \"$option\" ;;
+    esac
+done
 
-python $ROOT/src/wadhwaniai_/build-output.py \
-    --image-root $ROOT/data \
-    --source $source
+if [ ! $version ]; then
+    version=`ls -t $data/metadata/ | head --lines=1`
+fi
+for i in $data/metadata/$version/*; do
+    src=( ${src[@]} --source $i )
+done
+
+python $ROOT/src/wadhwaniai_/build-output.py ${src[@]} --data-root $data
