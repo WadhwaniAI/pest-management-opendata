fea4ac076b46761494fb8ddd0aa741e23112ee60
Jerome White <jerome@wadhwaniai.org>
2022-07-06 14:49:47 +0530

    Support for CLI options

--- bin/to-detectron2.sh
+++ bin/to-detectron2.sh
@@ -5,15 +5,30 @@ ROOT=`git rev-parse --show-toplevel`
 export PYTHONPATH=$ROOT:$PYTHONPATH
 export PYTHONLOGLEVEL=info
 
-splits=(
-    train
-    validation
-)
+while getopts 'd:v:h' option; do
+    case $option in
+	d) data=$OPTARG ;;
+        v) version=$OPTARG ;;
+        h)
+	    cat <<EOF
+Usage
+./$0 -v [version]
+EOF
+            exit 0
+            ;;
+        *) echo -e Unrecognized option \"$option\" ;;
+    esac
+done
+
+if [ ! $version ]; then
+    version=`ls -t $data/metadata/ | head --lines=1`
+fi
 
 tmp=`mktemp --directory`
-for i in ${splits[@]}; do
-    python $ROOT/src/detectron2_/build-output.py \
-	   --image-root $ROOT/data \
-	   --split $i > $tmp/$i.json
+for i in train val; do
+    zcat $data/metadata/$version/dev.csv.gz | \
+	python $ROOT/src/detectron2_/build-output.py \
+	       --data-root $data \
+	       --split $i > $tmp/$i.json
 done
 echo $tmp
